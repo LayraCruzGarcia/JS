b6a5c5d63b9acd3a2105d350dbb804ee6a207389
LayraCruzGarcia <133907113+LayraCruzGarcia@users.noreply.github.com>
2025-08-25 09:47:59 -0300

    Condições-Parte 01

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é  ${vel}`)
+if (vel > 60){//uma condição simples, quando não tem else.
+    console.log( 'Você ultrapassou a vecolidade permitida, você foi multado' )
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log (`Vivendo em ${país}`)
+if(pais != 'Brasil'){
+    console.log('Você é estrangeiro')
+} else{
+    console.log('Você é Brasileiro')
+}
